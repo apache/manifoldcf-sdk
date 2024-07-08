71b329d969c58654757083fbcb1849611de113a3
Piergiorgio Lucidi <plucidi@ziaconsulting.com>
2024-07-08 15:14:47 +0200

    added support for MCF Docker Repo, added debug port 5005, support for Maven deps publicly available

--- README.md
+++ README.md
@@ -1,19 +1,35 @@
-# Apache ManifoldCF SDK 0.0.4
+# Apache ManifoldCF SDK 0.0.5
 This is the SDK project of Apache ManifoldCF dedicated to developers that need to extend the platform with new components, connectors or integrations using Maven and Docker. On the other hand this SDK can be extremely useful also for all the developers who want to contribute to the ManifoldCF project.
 This project has started with an initial contribution by @OpenPj and @binduwavell.
 
 The SDK will take care of the following tasks:
+* Creating custom containers using the official ManifoldCF Docker Images
 * Creating the Docker Volumes for the MCF Maven Repo and the MCF installation
 * Preparing the Docker Image for executing the build installing Ant and Maven
 * Running the container for executing the building process with Ant and then with Maven
 * Copying the entire Maven Repo in the Maven target folder in the host machine (locally)
 * Generating the MCF official site
 
+ManifoldCF SDK is using the ManifoldCF Docker repository:
+[https://hub.docker.com/r/apache/manifoldcf](https://hub.docker.com/r/apache/manifoldcf)
+
+What's new in v0.0.5
+* The Maven profile `docker-hub` now is enabled by default
+* MCF SDK now by default is building the image from the official Docker images
+* Added a debug port (5005) for accelerate connectors development with remote debugging
+* Default Maven dependencies version now is 2.27-SNAPSHOT (publicly available)
+
+If you need to build your custom Docker image using dependencies built from the MCF source code and stored in the target folder (using the `init` script mentioned below), please use the `local` Maven profile.
+
 Examples of commands using the main bash script for a typical usage:
 * `./run.sh init 2.26 ga` -> start the init process for ManifoldCF 2.26 GA
-* `./run.sh build_start` -> build extensions and run everything with Docker locally
-* `./run.sh start` -> start all the containers (MCF and PostgreSQL)
-* `./run.sh stop` -> stop all the containers (MCF and PostgreSQL)
+* `./run.sh init custom-version local /<USER_HOME>/Documents/workspaces/manifoldcf/custom-project` -> start the init process for a custom ManifoldCF project
+* `./run.sh build_start` -> build extensions and run everything with Docker locally using an official ManifoldCF distribution
+* `./run.sh build_start_local` -> build extensions and run everything with Docker locally using a custom local ManifoldCF source code repository
+* `./run.sh start` -> start all the containers (MCF dist and PostgreSQL)
+* `./run.sh stop` -> stop all the containers (MCF dist and PostgreSQL)
+* `./run.sh start_local` -> start all the containers (custom MCF project and PostgreSQL)
+* `./run.sh stop_local` -> stop all the containers (custom MCF project and PostgreSQL)
 
 Examples of commands for developing and testing MCF itself:
 * `./run.sh init 2.22.1 rc` -> start the init process for ManifoldCF 2.22.1 RC
@@ -28,7 +44,7 @@ Examples of commands for generating the MCF website contents:
 * `./run-site.sh clean` -> Start the mcf-sdk-site in order to clean the project
 * `./run-site.sh stop` -> Stop the mcf-sdk-site
 
-# Initializing the local Maven repository using Docker
+# Initializing the local Maven repository using Docker (optional)
 In order to compile extensions with Maven, the SDK provides an initialization script that will download the source code of ManifoldCF and then build it in the container. At the end of the build process the Maven repository included in the container will be copied in the local folder `target/mcf-maven-repo`. The local Maven repo will be used as the main reference for compiling the extension code.
 
 The initialization command consists of the following arguments:
--- run.sh
+++ run.sh
@@ -1,14 +1,86 @@
 #!/bin/sh
 mcfversion=$2
 mcfdist=$3
+mcfdisturl=$4
 
-init() {
-	echo "Starting Apache ManifoldCF SDK initialization for version $mcfversion"
+initLocal() {
+	echo "Starting Apache ManifoldCF SDK initialization for version $mcfversion - Init Local"
 
 	mcfversionInFuction=$1
-	echo "Init - 1 = $1"
+	echo "Init - 1 - MCF version = $1"
+	
 	mcfdistInFunction=$2
-	echo "Init - 2 = $2"
+	echo "Init - 2 - MCF DIST URL = $2"
+	
+	mcfdistTypeInFunction=$3
+	echo "Init - 3 - MCF DIST TYPE = $3"
+
+	echo "Updating the ManifoldCF version inside the pom.xml"
+	mvn versions:set-property -Dproperty=manifoldcf.version -DnewVersion=$mcfversionInFuction
+    
+  echo "Creating Docker Volume for the ManifoldCF Maven Repository"
+  docker volume create --name mcf-maven-repo
+    
+  echo "Creating Docker Volume for the ManifoldCF Installation..."
+  docker volume create --name mcf-app-volume
+    
+  echo "Creating simlink for the local MCF source code repository: $mcfdistInFunction"
+  mkdir target
+  cd target 
+  ln -s $mcfdistInFunction mcf-local
+  cd ..
+
+  echo "Building the Docker Container for executing Ant and Maven build..."
+	docker build src/main/docker-local -t mcf-sdk-init
+
+	echo "Copying local source code into the container..."
+	docker run -itd --rm --name mcf-sdk-init-container -v mcf-maven-repo:/root/.m2 -v mcf-app-volume:/usr/src/manifoldcf -t mcf-sdk-init  bash
+  docker cp target/mcf-local/. mcf-sdk-init-container:/usr/src/manifoldcf
+  docker stop mcf-sdk-init-container
+	
+	echo "Starting the mcf-sdk-build-container for executing the Maven and Ant building process..."
+	docker run -it --rm --name mcf-sdk-init-container -v mcf-maven-repo:/root/.m2 -v mcf-app-volume:/usr/src/manifoldcf -t mcf-sdk-init /bin/bash -c "ant make-core-deps make-deps image; \
+	mvn install:install-file -DgroupId=org.apache.manifoldcf -DartifactId=mcf-api-service -Dversion=$mcfversion -Dpackaging=war -Dfile=/usr/src/manifoldcf/dist/web/war/mcf-api-service.war; \
+	mvn install:install-file -DgroupId=org.apache.manifoldcf -DartifactId=mcf-authority-service -Dversion=$mcfversion -Dpackaging=war -Dfile=/usr/src/manifoldcf/dist/web/war/mcf-authority-service.war; \
+	mvn install:install-file -DgroupId=org.apache.manifoldcf -DartifactId=mcf-combined-service -Dversion=$mcfversion -Dpackaging=war -Dfile=/usr/src/manifoldcf/dist/web/war/mcf-combined-service.war; \
+	mvn install:install-file -DgroupId=org.apache.manifoldcf -DartifactId=mcf-crawler-ui -Dversion=$mcfversion -Dpackaging=war -Dfile=/usr/src/manifoldcf/dist/web/war/mcf-crawler-ui.war
+	mvn -B -e -T 1C clean install -DskipTests -DskipITs"
+
+	echo "Starting the mcf-sdk-init-container to export the Maven repository in target/mcf-maven-repo..."
+	docker run --rm --name mcf-sdk-init-container -itd -v mcf-maven-repo:/root/.m2 -v mcf-app-volume:/usr/src/manifoldcf -t mcf-sdk-init bash
+	
+	echo "Copying the ManifoldCF Maven Repo in target/mcf-maven-repo..."
+	mkdir target
+	docker cp mcf-sdk-init-container:/root/.m2/repository target/mcf-maven-repo
+
+	mkdir target/mcf-dist
+	echo "Copying the mcf-dist in target/mcf-dist"
+	docker cp mcf-sdk-init-container:/usr/src/manifoldcf/dist/apache-manifoldcf-$mcfdistInFunction-dev-bin.tar.gz target/mcf-dist
+
+	tar -xvz target/mcf-dist/apache-manifoldcf-$mcfdistInFunction-dev-bin.tar.gz -C target/mcf-dist
+	rm apache-manifoldcf-$mcfdistInFunction-dev-bin.tar.gz
+	
+	echo "Stopping and removing the existent building container..."
+	docker stop mcf-sdk-init-container
+	
+	#echo "Merging the ManifoldCF Maven Repository with the local Maven Repo"
+	#cp -n target/mcf-maven-repo $HOME/.m2
+}
+
+init226() {
+	echo "Starting Apache ManifoldCF SDK initialization for version $mcfversion - Init 2.26"
+
+	mcfversionInFuction=$1
+	echo "Init - 1 - MCF version = $1"
+
+	mcfdistInFunction=$2
+	echo "Init - 2 - MCF DIST URL = $2"
+	
+	mcfdistTypeInFunction=$3
+	echo "Init - 3 - MCF DIST TYPE = $3"
+
+	echo "Updating the ManifoldCF version inside the pom.xml"
+	mvn versions:set-property -Dproperty=manifoldcf.version -DnewVersion=$mcfversionInFuction
     
   echo "Creating Docker Volume for the ManifoldCF Maven Repository"
   docker volume create --name mcf-maven-repo
@@ -18,7 +90,8 @@ init() {
     
   echo "Building the Docker Container for executing Ant and Maven build..."
 	docker build --build-arg MCF_VERSION=$mcfversionInFuction --build-arg MCF_DIST_URL=$mcfdistInFunction src/main/docker -t mcf-sdk-init
-
+	
+  
 	echo "Starting the mcf-sdk-build-container for executing the Maven and Ant building process..."
 	docker run -it --rm --name mcf-sdk-init-container -v mcf-maven-repo:/root/.m2 -v mcf-app-volume:/usr/src/manifoldcf -t mcf-sdk-init /bin/bash -c "sed -i -e 's/<fileset dir=\"test-materials\" excludes/<fileset dir=\"test-materials\" erroronmissingdir=\"false\" excludes/g' connectors/solr/build.xml; \
 	sed -i -e 's/<?xml version=\"1\.0\" encoding=\"UTF-8\"?>/ /g' connectors/csv/pom.xml; \
@@ -31,7 +104,7 @@ init() {
 	mvn install:install-file -DgroupId=org.apache.manifoldcf -DartifactId=mcf-combined-service -Dversion=$mcfversion -Dpackaging=war -Dfile=/usr/src/manifoldcf/dist/web/war/mcf-combined-service.war; \
 	mvn install:install-file -DgroupId=org.apache.manifoldcf -DartifactId=mcf-crawler-ui -Dversion=$mcfversion -Dpackaging=war -Dfile=/usr/src/manifoldcf/dist/web/war/mcf-crawler-ui.war
 	mvn -B -e -T 1C clean install -DskipTests -DskipITs"
-	
+  
 	echo "Starting the mcf-sdk-init-container to export the Maven repository in target/mcf-maven-repo..."
 	docker run --rm --name mcf-sdk-init-container -itd -v mcf-maven-repo:/root/.m2 -v mcf-app-volume:/usr/src/manifoldcf -t mcf-sdk-init bash
 	
@@ -46,9 +119,65 @@ init() {
 	#cp -n target/mcf-maven-repo $HOME/.m2
 }
 
+init() {
+	echo "Starting Apache ManifoldCF SDK initialization for version $mcfversion - Init"
+
+	mcfversionInFuction=$1
+	echo "Init - 1 - MCF version = $1"
+
+	mcfdistInFunction=$2
+	echo "Init - 2 - MCF DIST URL = $2"
+	
+	mcfdistTypeInFunction=$3
+	echo "Init - 3 - MCF DIST TYPE = $3"
+
+	echo "Updating the ManifoldCF version inside the pom.xml"
+	mvn versions:set-property -Dproperty=manifoldcf.version -DnewVersion=$mcfversionInFuction
+    
+  echo "Creating Docker Volume for the ManifoldCF Maven Repository"
+  docker volume create --name mcf-maven-repo
+    
+  echo "Creating Docker Volume for the ManifoldCF Installation..."
+  docker volume create --name mcf-app-volume
+    
+  echo "Building the Docker Container for executing Ant and Maven build..."
+	docker build --build-arg MCF_VERSION=$mcfversionInFuction --build-arg MCF_DIST_URL=$mcfdistInFunction src/main/docker -t mcf-sdk-init
+	
+	echo "Starting the mcf-sdk-build-container for executing the Maven and Ant building process..."
+	docker run -it --rm --name mcf-sdk-init-container -v mcf-maven-repo:/root/.m2 -v mcf-app-volume:/usr/src/manifoldcf -t mcf-sdk-init /bin/bash -c "ant make-core-deps make-deps build; \
+	mvn install:install-file -DgroupId=org.apache.manifoldcf -DartifactId=mcf-api-service -Dversion=$mcfversion -Dpackaging=war -Dfile=/usr/src/manifoldcf/dist/web/war/mcf-api-service.war; \
+	mvn install:install-file -DgroupId=org.apache.manifoldcf -DartifactId=mcf-authority-service -Dversion=$mcfversion -Dpackaging=war -Dfile=/usr/src/manifoldcf/dist/web/war/mcf-authority-service.war; \
+	mvn install:install-file -DgroupId=org.apache.manifoldcf -DartifactId=mcf-combined-service -Dversion=$mcfversion -Dpackaging=war -Dfile=/usr/src/manifoldcf/dist/web/war/mcf-combined-service.war; \
+	mvn install:install-file -DgroupId=org.apache.manifoldcf -DartifactId=mcf-crawler-ui -Dversion=$mcfversion -Dpackaging=war -Dfile=/usr/src/manifoldcf/dist/web/war/mcf-crawler-ui.war
+	mvn -B -e -T 1C clean install -DskipTests -DskipITs"
+  
+
+	echo "Starting the mcf-sdk-init-container to export the Maven repository in target/mcf-maven-repo..."
+	docker run --rm --name mcf-sdk-init-container -itd -v mcf-maven-repo:/root/.m2 -v mcf-app-volume:/usr/src/manifoldcf -t mcf-sdk-init bash
+	
+	echo "Copying the ManifoldCF Maven Repo in target/mcf-maven-repo..."
+	mkdir target
+	docker cp mcf-sdk-init-container:/root/.m2/repository target/mcf-maven-repo
+	
+	echo "Stopping and removing the existent building container..."
+	docker stop mcf-sdk-init-container
+	
+	#echo "Merging the ManifoldCF Maven Repository with the local Maven Repo"
+	#cp -n target/mcf-maven-repo $HOME/.m2
+}
+
+
+
 build_start() {
 	echo "Building with Maven skipping tests and run Docker containers"
 	mvn clean install docker:build docker:start -DskipTests -DskipITs
+}
+
+build_start_local() {
+	echo "Building with Maven skipping tests and run Docker containers"
+	mkdir target/mcf-dist
+	cp -R target/mcf-local/dist/. target/mcf-dist
+	mvn clean install docker:build docker:start -DskipTests -DskipITs -Plocal
 }
 
 build() {
@@ -56,9 +185,19 @@ build() {
 	mvn clean install -DskipTests -DskipITs
 }
 
+build_local() {
+	echo "Building with Maven skipping tests"
+	mvn clean install -DskipTests -DskipITs -Plocal
+}
+
 build_test() {
 	echo "Building with Maven executing tests"
 	mvn clean install
+}
+
+build_test_local() {
+	echo "Building with Maven executing tests"
+	mvn clean install -Plocal
 }
 
 unittests() {
@@ -71,14 +210,29 @@ integrationtests() {
 	mvn clean integration-test
 }
 
+integrationtests_local() {
+	echo "Executing integration tests"
+	mvn clean integration-test -Plocal
+}
+
 start() {
 	echo "Run ManifoldCF Docker containers"
 	mvn docker:start
 }
 
+start_local() {
+	echo "Run ManifoldCF Docker containers"
+	mvn docker:start -Plocal
+}
+
 stop() {
 	echo "Stop ManifoldCF Docker containers"
 	mvn docker:stop
+}
+
+stop_local() {
+	echo "Stop ManifoldCF Docker containers"
+	mvn docker:stop -Plocal
 }
 
 test() {
@@ -100,30 +254,46 @@ purge() {
 
 case "$1" in
   init)
-    if [ ! -n "$mcfversion" ]
+    if [ ! -n "$mcfversion" ] && [ "$mcfdist" != "local" ];
 	then
-	    echo "$0 - Error ManifoldCF version is not set. Usage for using ManifoldCF 2.26: $0 init 2.26 ga"
+	    echo "$0 - Error ManifoldCF version is not set. Usage for using ManifoldCF 2.26: $0 init 2.26 ga or $0 init 2.27 rc"
 	    exit 1
 	else
 	    echo "ManifoldCF version is correctly set"
 	fi
-	if [ ! -n "$mcfdist" ]
+	if [ ! -n "$mcfdist" ];
 	then
-	    echo "$0 - Error ManifoldCF release type is not set, it should be ga or rc. Usage for using ManifoldCF 2.21: $0 init 2.21 ga or $0 init 2.21 rc"
+	    echo "$0 - Error ManifoldCF release type is not set, it should be ga or rc. Usage for using ManifoldCF 2.21: $0 init 2.21 ga or $0 init 2.21 rc or $0 init local YOUR_LOCAL_PATH"
 	    exit 1
 	else
 	    echo "ManifoldCF release type is correctly set and now starting Apache ManifoldCF SDK initialization script..."
 	fi
-	if [ "$mcfdist"=="ga" ];
-	mcfdisturl=""
-	then
+
+	if [ "$mcfdist" -eq "ga" ];
+		then
 		mcfdisturl="https://dlcdn.apache.org/manifoldcf"
 		echo "Building a GA release with artifact base URL: $mcfdisturl"
-	else
+	elif [ "$mcfdist" -eq "rc" ];
+		then
 		mcfdisturl="https://dist.apache.org/repos/dist/dev/manifoldcf"
 		echo "Building a RC release with artifact base URL: $mcfdisturl"
+	elif [ "$mcfdist" -eq "local" ];
+		then
+		echo "Building a local build: $mcfdisturl"
 	fi
-    init $mcfversion $mcfdisturl
+
+	if [ "$mcfdist"=="ga" ] && [ "$mcfversion" -eq "2.26" ];
+    then
+    	echo "mcfversion=$mcfversion | mcfdisturl: $mcfdisturl | mcfdist: $mcfdist"
+    	init226 $mcfversion $mcfdisturl $mcfdist
+  elif [ "$mcfdist"=="local" ];
+  	then
+  		echo "mcfversion=$mcfversion | mcfdisturl: $mcfdisturl | mcfdist: $mcfdist"
+  		initLocal $mcfversion $mcfdisturl $mcfdist
+  else
+  	 echo "mcfversion=$mcfversion | mcfdisturl: $mcfdisturl | mcfdist: $mcfdist"
+  	 init $mcfversion $mcfdisturl $mcfdist
+  fi
     ;;
   clean)
     clean
@@ -131,17 +301,29 @@ case "$1" in
   build_start)
 		build_start
 		;;
+	build_start_local)
+		build_start_local
+		;;
 	start)
 		start
 		;;
+	start_local)
+		start_local
+		;;
 	stop)
 		stop
+		;;
+	stop_local)
+		stop_local
 		;;
 	unittests)
 		unittests
 		;;
 	integrationtests)
 		integrationtests
+		;;
+	integrationtests_local)
+		integrationtests_local
 		;;
   test)
     test
@@ -150,5 +332,5 @@ case "$1" in
     purge
     ;;
   *)
-    echo "Usage: $0 {init|clean|build_start|start|stop|unittests|integrationtests|test|purge}"
+    echo "Usage: $0 {init|clean|build_start|build_start_local|start|start_local|stop|stop_local|unittests|integrationtests|integrationtests_local|test|purge}"
 esac
